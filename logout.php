d30a4d7c22f6cc1c4228834c4d3c52bf4fa486e1
atharva694 <atharvasutar694@gmail.comm>
2026-07-25 10:44:47 +0530

    fresh start

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/includes/auth_functions.php';
-
-auth_logout();
-auth_redirect('login.php', 'You have been logged out successfully.', 'success');
